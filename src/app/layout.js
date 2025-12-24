6de2aab45d9c9f3df1548baec957a9ca65562003
jarodlopez <127226881+jarodlopez@users.noreply.github.com>
2025-12-23 22:01:48 -0600

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { CartProvider } from '../context/CartContext';
+import Navbar from '../components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Tienda Online',
+  description: 'Creado con Next.js y Firebase',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>
+        <CartProvider>
+          <Navbar />
+          {children}
+        </CartProvider>
+      </body>
+    </html>
+  );
+}

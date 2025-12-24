f91a6856ae975f4a7b2d7f4ebea9b9f24693f21c
jarodlopez <127226881+jarodlopez@users.noreply.github.com>
2025-12-23 21:56:49 -0600

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.ibb.co', 'firebasestorage.googleapis.com'], // Permite imágenes de imgbb y firebase
+  },
+}
+
+module.exports = nextConfig
